1cde501d884014eb47c29b7c53e64df1d3a8e7ad
feove <feove31@gmail.com>
2024-09-01 21:41:06 +0200

    Add a window

---
+++ binary_puzzle_tkinter.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()  
+root.title("Tk Example")
+root.configure(background="white")
+root.minsize(500, 500) 
+root.maxsize(500, 500)
+root.geometry("300x300+50+50")  
+root.mainloop()
